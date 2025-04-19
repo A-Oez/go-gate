050282b4b306889ecd085a874b3f25b1d0698697
AOez <oezahm60@gmail.com>
2025-04-20 01:42:08 +0200

    added rate limiting per client

--- internal/server/middleware/limiter/limiter.go
+++ internal/server/middleware/limiter/limiter.go
@@ -3,26 +3,52 @@ package limiter
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
 type message struct{
 	Status string
-	Body string
+	Msg string
 }
 
-//global rate limiter 
+var (
+	ipLimiters = make(map[string]*rate.Limiter)
+	mu        sync.Mutex
+)
+
 func RateLimiter(next http.Handler) http.Handler {
-	//TODO add logic to implement rate limiting per client
-	limiter := rate.NewLimiter(5, 10)
+	go func() {
+		for {
+			time.Sleep(15 * time.Minute)
+
+			mu.Lock()
+			for ip := range ipLimiters {
+				delete(ipLimiters, ip)
+			}
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip := r.RemoteAddr
+
+		mu.Lock()
+		defer mu.Unlock()
+
+		if _, exists := ipLimiters[ip]; !exists {
+			ipLimiters[ip] = rate.NewLimiter(5, 10)
+		}
+
+		limiter := ipLimiters[ip]
 		if limiter.Allow() {
 			next.ServeHTTP(w, r)
 		} else {
 			msg := message{
                 Status: "Request Failed",
-                Body:   "Try again later.",
+                Msg:   "Try again later",
             }
 
             w.WriteHeader(http.StatusTooManyRequests)
