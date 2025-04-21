297b34b460f5997495cd3eed25bf3cfa35667ef4
AOez <oezahm60@gmail.com>
2025-04-21 20:48:01 +0200

    rework mapping entity

--- internal/db/entity/mapping/mapping_entity.go
+++ internal/db/entity/mapping/mapping_entity.go
@@ -3,7 +3,7 @@ package mapping
 type ProxyMapping struct {
 	ID            int    `json:"id"`
 	Method        string `json:"method"`
-	PublicPath    string `json:"client_path"`
+	PublicPath    string `json:"public_path"`
 	ServiceScheme string `json:"service_scheme"`
 	ServiceHost   string `json:"service_host"`
 	ServicePath   string `json:"service_path"`
