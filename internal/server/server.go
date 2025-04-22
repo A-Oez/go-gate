cd86ad518d0d7cde26c632db831e7324c9b7cb5f
AOez <oezahm60@gmail.com>
2025-04-22 19:16:40 +0200

    rework server start method

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
+
+	"github.com/fatih/color"
 )
 
 type Server struct {
@@ -18,6 +21,27 @@ func NewServer(db *sql.DB) *Server {
 }
 
 func (s *Server) Start(port string) {
-	s.registerRouter()
-	http.ListenAndServe(port, s.Mux)
+    s.registerRouter()
+
+    str := `
+                                       __          
+   ____   ____             _________ _/  |_  ____  
+  / ___\ /  _ \   ______  / ___\__  \\   __\/ __ \ 
+ / /_/  >  <_> ) /_____/ / /_/  > __ \|  | \  ___/ 
+ \___  / \____/          \___  (____  /__|  \___  >
+/_____/                 /_____/     \/          \/ 
+	`
+
+    green := color.New(color.FgGreen).SprintFunc()
+    cyan := color.New(color.FgCyan).SprintFunc()
+	blue := color.New(color.FgBlue).SprintFunc()
+
+    log.Printf("%s\n%s %s\n%s %s", 
+		blue(str), 
+		green("Server is running on: 🚀 "), 
+		cyan("http://127.0.0.1" + port),
+		green("Find the source code here: 🔗 "),
+		cyan("https://github.com/A-Oez/go-gate"))
+
+    http.ListenAndServe(port, s.Mux)
 }
